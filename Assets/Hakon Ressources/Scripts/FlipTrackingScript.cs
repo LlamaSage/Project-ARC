11684da4cd0ca45a2c9d57c5a05313d79dbc3290
Unknown <shigure@hotmail.de>
2018-01-11 19:35:22 +0100

    Project Push
    Uploading the actual project

---
+++ Assets/Hakon Ressources/Scripts/FlipTrackingScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class FlipTrackingScript : MonoBehaviour
+{
+    private void Start()
+    {
+        VuforiaBehaviour.Instance.enabled = false;
+    }
+
+    public void changeTracking(bool track)
+    {
+        VuforiaBehaviour.Instance.enabled = track;
+    }
+}
---
+++ Assets/Hakon Ressources/Scripts/SceneLoaderScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderScript : MonoBehaviour {
+
+    public int loadSceneIndex;
+    private float startTime = 0;
+    private Quaternion startRotation;
+
+    public void Start()
+    {
+        startRotation = transform.rotation;
+    }
+
+    public void OnMouseDown()
+    {
+        startTime = Time.time;
+    }
+
+    public void OnMouseUp()
+    {
+        Debug.Log("UP at " + (Time.time - startTime));
+        if(Time.time - startTime >= 2.0f)
+        {
+            SceneManager.LoadScene(loadSceneIndex);
+        }
+        else
+        {
+            transform.rotation = startRotation;
+        }
+    }
+
+    public void LoadSceneDirectly()
+    {
+        SceneManager.LoadScene(loadSceneIndex);
+    }
+
+    public void OnMouseDrag()
+    {
+        if(Time.time - startTime < 2.0f)
+        {
+            
+            transform.Rotate(new Vector3(1, 1, 1));
+        }
+    }
+}
